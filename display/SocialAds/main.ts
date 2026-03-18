8ce0bb2c146677a19c3556b8170577660c6af55d
lazarsto-create <Lazarsto@gmail.com>
2026-03-18 13:07:29 +0100

    fix: ispravljena struktura

--- display/SocialAds/main.ts
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("The project is currently empty or does not contain any TypeScript files.");
-    // Any additional setup or interaction with the DOM can go here.
-});
